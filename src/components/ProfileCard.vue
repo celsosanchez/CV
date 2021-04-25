35f73354fe519b284f89011c4a5900ac174b07c0
celso <celso.sanchezviera@gmail.com>
2021-04-25 23:23:06 +0200

    Initial Commit

---
+++ src/components/ProfileCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-card class="mx-auto" max-width="434" tile>
+    <v-img
+      height="100%"
+      src="https://cdn.vuetifyjs.com/images/cards/server-room.jpg"
+    >
+      <v-row align="end" class="fill-height">
+        <v-col align-self="start" class="pa-0" cols="12">
+          <v-avatar class="profile" color="grey" size="164" tile>
+            <v-img
+            
+              lazy-src="https://picsum.photos/id/11/10/6"
+              max-height="340"
+              max-width="300"
+              src="@/assets/Photo.png"
+            ></v-img>
+          </v-avatar>
+        </v-col>
+        <v-col class="py-0">
+          <v-list-item color="rgba(0, 0, 0, .4)" dark>
+            <v-list-item-content>
+              <v-list-item-title class="title">
+                Celso Sanchez
+              </v-list-item-title>
+              <v-list-item-subtitle>Informatics Engineer</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-col>
+      </v-row>
+    </v-img>
+  </v-card>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="home">
+    <profile-card/>
+    <hello-world/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+   
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import axios from "axios";
+import HelloWorld from '../components/HelloWorld.vue';
+import ProfileCard from '@/components/ProfileCard.vue';
+
+
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+    ProfileCard
+   
+  },
+  data() {
+    return {
+      products:[],
+      user:{}
+    };
+  },
+  methods: {
+    getProd() {
+      var url = "http://localhost:3000/products";
+      axios
+        .get(url)
+        .then((res) => {
+          this.products = res.data.found;
+          this.user = res.data.user;
+        })
+        .then(() => {
+          // this.activestory = this.tales[0];
+        });
+    },
+  },
+  created(){
+    this.getProd()
+  }
+};
+</script>
